1b096eb7cae5be648ae5319210f3b1124a1b70aa
Om Mandaliya <ommandaliya83@gmail.com>
2026-07-20 18:00:27 +0000

    commit

--- variable.js
+++ variable.js
@@ -1,8 +1,10 @@
 const accountId = 2345
 let email = "hello@gmail.com"
 var password = "123abc"
-
+name = "om"
+// we also declare variable without any data type in js but it is not efficient solution and we don't use for that
 email = "om@gmail.com"
 password = "hello"
+let accountstate;
 // accountId = 234 do not change const value at run time 
-console.table([accountId,email,password])
+console.table([accountId,email,password,name,accountstate])
