628988d98c5920712f92228e60c3dbce1357f1ce
Om Mandaliya <ommandaliya83@gmail.com>
2026-07-20 16:21:58 +0000

    commit

---
+++ variable.js
@@ -0,0 +1,8 @@
+const accountId = 2345
+let email = "hello@gmail.com"
+var password = "123abc"
+
+email = "om@gmail.com"
+password = "hello"
+// accountId = 234 do not change const value at run time 
+console.table([accountId,email,password])
