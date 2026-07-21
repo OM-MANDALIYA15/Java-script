1cf1102bfc1a7d4edec570e30cbb9c4392ee1231
Om Mandaliya <ommandaliya83@gmail.com>
2026-07-21 13:22:44 +0000

    commit

---
+++ Data_types.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// Data types define the type of value which kind of variable can be stored 
+// JS have vary limited types of data type
+
+let a = 10; // this is a integer data type
+console.log(a)
+let b
+console.log(b)
+
+console.log(typeof a)
+console.log(typeof undefined) // type is undefined
+console.log(typeof null) // type is OBJECT
+// Types
+// number = 2 to power 23
+// bidint
+// boolean => True or False
+// undefined => 
+// null
+//symbol => used for unique
+
+
+//object 
