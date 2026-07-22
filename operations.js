f804ff57cb79c7239393af01afc35a19e3231e54
Om Mandaliya <ommandaliya83@gmail.com>
2026-07-22 13:58:49 +0000

    commit

---
+++ operations.js
@@ -0,0 +1,14 @@
+console.log(2+ "2")
+console.log("2" + 4)
+console.log(2 + 2 + "4")
+
+console.log(+true)
+console.log(+false)
+
+let a = 101;
+a++
+console.log(a)
+
+let b = 101;
+++b
+console.log(b)
