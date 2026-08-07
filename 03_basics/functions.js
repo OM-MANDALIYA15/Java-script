c01e837d2d6c65b1da60e6eb9bd73e848391ce8a
Om Mandaliya <ommandaliya83@gmail.com>
2026-08-07 06:32:43 +0000

    commit

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -20,8 +20,46 @@ function add(n1,n2)
 const result1 = add(5,2)
 // console.log(result1)
 
-function user(username)
+function user(username = "om")
 {
+    if(!username)
+    {
+        console.log("please enter a username")
+        return
+    }
     return `${username} just logged in`
 }
-user("om")
+// console.log(user(12))
+
+// console.log(user()); // undefined just logged in
+
+function calculate(...num1)
+{
+    return num1
+}
+console.log(calculate(200,200,300))
+
+// Function using objects
+const user1 = { 
+    name : " om",
+    id : 20
+}
+function fun1(anyObject)
+{
+    console.log(`Username is ${anyObject.name} and price is ${anyObject.id}`)
+
+}
+// fun1(user1)
+fun1({
+    name : " akshar",
+    id : 20
+})
+
+// Function using of array
+const Array = [123,321,233]
+function value(getArray)
+{
+    return getArray[1]
+}
+console.log(value(Array))
+console.log(value([12,32,33]))
